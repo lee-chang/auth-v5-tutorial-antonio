4d4a430da4729324e14fbfc8792b770bd40eb527
Lee <ca.y25@hotmail.com>
2024-01-04 17:07:38 -0500

    Update auth.ts with session and jwt callbacks

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,32 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { type DefaultSession } from "next-auth";
+import {JWT} from "@auth/core/jwt";
+import authConfig from "./auth.config";
 import { db } from "./lib/db";
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  signOut
+  signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as "USER" | "ADMIN";
+      }
+
+      return session;
+    },
+    async jwt(token) {
+      console.log(token);
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
